5f8544d23d394affbf730fadc456efe6a4128d9b
notnulldev <notnulldev0@gmail.com>
2022-12-26 15:17:53 +0100

    update

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/labstack/echo/v4"
+)
+
+const (
+	configFolder = ".nano-cicd"
 )
 
 type BuildArguments struct {
@@ -48,16 +56,73 @@ func build(c echo.Context) error {
 		})
 	}
 
-	content, err := os.ReadFile("Dockerfile")
+	err = runPreDeployScript()
+
 	if err != nil {
 		return c.JSON(400, ErrorResponse{
 			Error: err.Error(),
 		})
 	}
 
-	println(string(content))
+	runDeployScript()
+
+	err = runPostDeployScript()
+
+	if err != nil {
+		return c.JSON(400, ErrorResponse{
+			Error: err.Error(),
+		})
+	}
 
 	return c.JSON(200, "")
+}
+
+func runPreDeployScript() error {
+	_, err := os.Stat(fmt.Sprintf("./%s/pre-deploy.sh", configFolder))
+
+	if err != nil {
+		log.Println("Could not read pre-deploy.sh script.")
+		return nil
+	}
+	return executeCommand(fmt.Sprintf("bash ./%s/pre-deploy.sh", configFolder))
+}
+
+func runDeployScript() error {
+	_, err := os.Stat(fmt.Sprintf("./%s/deploy.sh", configFolder))
+
+	if err != nil {
+		log.Println("Could not read deploy.sh script.")
+		return nil
+	}
+
+	return executeCommand(fmt.Sprintf("bash ./%s/deploy.sh", configFolder))
+}
+
+func runPostDeployScript() error {
+	_, err := os.Stat(fmt.Sprintf("./%s/post-deploy.sh", configFolder))
+
+	if err != nil {
+		log.Println("Could not read post-deploy.sh script.")
+		return nil
+	}
+	return executeCommand(fmt.Sprintf("bash ./%s/post-deploy.sh", configFolder))
+}
+
+func executeCommand(command string) error {
+	splitted := strings.Split(command, " ")
+
+	if len(splitted) <= 1 {
+		return errors.New("could not split command")
+	}
+
+	cmd := exec.Command(splitted[0], splitted[1:]...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
+	return err
 }
 
 func cloneRepo(repoUrl string) error {
