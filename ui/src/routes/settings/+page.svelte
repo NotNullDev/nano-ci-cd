e2a1e810459dc834dd5cba8c410e57b8fbd8d446
notnulldev <notnulldev0@gmail.com>
2023-02-13 16:56:23 +0100

    settings page layout testing

--- ui/src/routes/settings/+page.svelte
+++ ui/src/routes/settings/+page.svelte
@@ -1,3 +1,13 @@
 <svelte:head>
 	<title>Settings</title>
 </svelte:head>
+
+<div class="flex flex-1 m-4 border border-b">
+	<div class="flex">
+		<div class="w-[200px] p-8 border m-2 mr-0 border-r-0 resize-x">panel 1</div>
+		<div class="bg-slate-300 w-[1px] my-2  relative">
+			<div class="absolute  w-[20px] right-0 top-0 h-full translate-x-[21px] cursor-e-resize" />
+		</div>
+	</div>
+	<div class="flex-1 p-8 border m-2">panel 2</div>
+</div>
